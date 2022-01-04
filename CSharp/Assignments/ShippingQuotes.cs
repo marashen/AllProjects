a9993681cbd7acae05a6387617771290f4573054
marashen <90586106+marashen@users.noreply.github.com>
2022-01-04 09:22:25 -0500

    Update ShippingQuotes.cs

--- CSharp/Assignments/ShippingQuotes.cs
+++ CSharp/Assignments/ShippingQuotes.cs
@@ -1,32 +1,51 @@
-Console.WriteLine("Welcome to Package Express!");
-Console.WriteLine("Please Enter the Package Weight:");
-int weight = Console.ReadLine();
-if (weight > 50) {
-  Console.WriteLine("To Heavy!!!");
-  }
-else {
-  Console.WriteLine("Please Enter the Package Width:");
-  int width = Console.ReadLine();
-  if (width > 40) {
-  Console.WriteLine("To Wide!!!");
-  }
-  else {
-    Console.WriteLine("Please Enter the Package Height:");
-    int height = Console.ReadLine();
-    if (height > 55) {
-    Console.WriteLine("To Tall!!!");
+using System;
+
+int looper = 1;
+while (looper == 1)
+{
+    Console.WriteLine("Welcome to Package Express!");
+    Console.Write("Please Enter the Package Weight:");
+    string weightIn = Console.ReadLine();
+    int weightOut = Convert.ToInt32(weightIn);
+    if (weightOut > 50)
+    {
+        Console.WriteLine("To Heavy!!!");
     }
-    else {
-      Console.WriteLine("Please Enter the Package Length:");
-      int length = Console.ReadLine();
-      if (length > 55) {
-      Console.WriteLine("To Long!!!");
-      }
-      else {
-      int tpt = height * length * width * weight
-      int quote = tpt / 100
-      Console.WriteLine("Your estimated total is: $" + quote);
-      }
+    else
+    {
+        Console.Write("Please Enter the Package Width:");
+        string widthIn = Console.ReadLine();
+        int widthOut = Convert.ToInt32(widthIn);
+        if (widthOut > 40)
+        {
+            Console.WriteLine("To Wide!!!");
+        }
+        else
+        {
+            Console.Write("Please Enter the Package Height:");
+            string heightIn = Console.ReadLine();
+            int heightOut = Convert.ToInt32(heightIn);
+            if (heightOut > 55)
+            {
+                Console.WriteLine("To Tall!!!");
+            }
+            else
+            {
+                Console.Write("Please Enter the Package Length:");
+                string lengthIn = Console.ReadLine();
+                int lengthOut = Convert.ToInt32(heightIn);
+                if (lengthOut > 55)
+                {
+                    Console.WriteLine("To Long!!!");
+                }
+                else
+                {
+                    int tpt = heightOut * lengthOut * widthOut * weightOut;
+                    int quote = tpt / 100;
+                    Console.WriteLine("Your estimated total is: $" + quote);
+                    looper -= 1;
+                }
+            }
+        }
     }
-  }
 }
